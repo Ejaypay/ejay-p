0fca64ec105b8596a905bc59d4591698744f0526
Ejaypay <earlinjan0126@gmail.com>
2024-09-13 21:15:07 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDetails() {
-    var details = document.getElementById("moreDetails");
-    if (details.style.display === "none") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
